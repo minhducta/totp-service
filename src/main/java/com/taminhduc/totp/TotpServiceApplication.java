00c57fa62b141ab8ad5ee29bf77a1cc3042c9861
ductm <ductm4@tcbs.com.vn>
2022-02-17 09:57:00 +0700

    First Upload SC

---
+++ src/main/java/com/taminhduc/totp/TotpServiceApplication.java
@@ -0,0 +1,13 @@
+package com.taminhduc.totp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TotpServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TotpServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/taminhduc/totp/TotpServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.taminhduc.totp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TotpServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
